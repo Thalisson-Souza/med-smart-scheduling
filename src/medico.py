d19b2be83802c9620377272ac85aa08cc646d284
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-25 03:19:00 -0300

    feat: validar disponibilidade de horário do médico

---
+++ src/medico.py
@@ -0,0 +1,11 @@
+from datetime import datetime, date, time
+
+class Medico:
+    def __init__(self, nome, hora_inicio, hora_fim):
+        self.__nome = nome
+        self.__hora_inicio = hora_inicio
+        self.__hora_fim = hora_fim
+
+
+    def esta_disponivel_no_horario(self, horario):
+        return self.__hora_inicio <= horario < self.__hora_fim
