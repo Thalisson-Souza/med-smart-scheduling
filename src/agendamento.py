218e9e977b8bb17cd37fa75b31e4f6717471486b
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-25 17:19:24 -0300

    refactor(agendamento): extrair métodos para realizar uma unica ação

--- src/agendamento.py
+++ src/agendamento.py
@@ -1,7 +1,6 @@
 from datetime import date, time
-
+from src.medico import Medico
 class Agendamento:
-    
     agenda = {
         date(2026, 4, 30): {
             time(8,0): True,
@@ -11,16 +10,24 @@ class Agendamento:
         }
     }
 
-    def processar_agendamento(self, data_agendamento, horario_agendamento):
-    
-        if data_agendamento not in self.agenda:
+    def _validar_data_do_agendamento(self, data):
+        if data not in self.agenda:
             raise ValueError("data não encontrada para agendamento")
         
-        if horario_agendamento not in self.agenda[data_agendamento]:
+    def _validar_horario_do_agendamento(self, horario, data):
+        if horario not in self.agenda[data]:
             raise ValueError("horário não existe para a data informada")
-        
-        if not self.agenda [data_agendamento][horario_agendamento]:
+
+    def _verificar_se_horario_esta_disponivel(self, data, horario):
+        if not self.agenda [data][horario]:
             raise ValueError("horário indisponivel para agendamento")
+        
+
+    def processar_agendamento(self, data_agendamento, horario_agendamento):
+        self._validar_data_do_agendamento(data_agendamento)
+        self._validar_horario_do_agendamento(horario_agendamento, data_agendamento)
+        self._verificar_se_horario_esta_disponivel(data_agendamento, horario_agendamento)
+
 
         return True
 
