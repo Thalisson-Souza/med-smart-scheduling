93a2a88433b04636ac84ddae8ba706f0cc03208f
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-25 14:27:00 -0300

    feat: add testes para validar se o intervalo de trabalho está condizente

--- tests/medico/test_medico.py
+++ tests/medico/test_medico.py
@@ -8,3 +8,19 @@ class TestMedico(TestCase):
         medico = Medico("Dr. House", time(8,0), time(12,0))
 
         self.assertFalse(medico.esta_disponivel_no_horario(time(14,00)))
+
+    def test_deve_permitir_registrar_agendamento_dentro_do_horario_do_atendimento(self):
+        medico = Medico("Dr. House", time(8,0), time(12,0))
+
+        self.assertTrue(medico.esta_disponivel_no_horario(time(9,00)))
+
+    def test_deve_permitir_registrar_agendamento_no_limite_inferior_do_horario_de_trabalho(self):
+        medico = Medico("Dr. House", time(8,0), time(12,0))
+
+        self.assertTrue(medico.esta_disponivel_no_horario(time(8,0)))
+
+    def test_deve_permitir_registrar_agendamento_no_limite_superior_do_horario_de_trabalho(self):
+        medico = Medico("Dr. House", time(8,0), time(12,0))
+
+        self.assertFalse(medico.esta_disponivel_no_horario(time(12,0)))
+
