431a2a36398dc7dac6c50305712344d9f2788fdb
Thalisson-Souza <thalisson.souza@acad.ufsm.br>
2026-04-29 14:26:37 -0300

    test: criação de paciente no sistema

--- tests/paciente/test_paciente.py
+++ tests/paciente/test_paciente.py
@@ -8,6 +8,7 @@ class TestPaciente(TestCase):
         with self.assertRaises(ValueError):
             Paciente(nome=None, cpf="00011122233")
 
-    def test_nao_deve_criar_paciente_sem_cpf(self):
-        with self.assertRaises(ValueError):
-            Paciente(nome="João", cpf=None)
+    def test_deve_criar_paciente_com_sucesso(self):
+        resultado = Paciente(nome="João", cpf="00011122233")
+        
+        self.assertTrue(resultado)
